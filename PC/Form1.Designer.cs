1c36ba95704e3ce0510be7a6fb9c6551ef6a3985
steveLim0928 <48382745+steveLim0928@users.noreply.github.com>
2020-07-24 15:00:08 +0800

    Add files via upload

---
+++ PC/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Arduino_Serial_Pc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Button 1";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 36);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 1;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(141, 36);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 26);
+            this.textBox6.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(154, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Button 2";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(141, 68);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(393, 195);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox PortSelection;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtSomething;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ PC/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arduino_Serial_Pc
+{
+    public partial class Form1 : Form
+    {
+        //object serialport to listen usb
+        System.IO.Ports.SerialPort Port;
+
+        //variable to check if arduino is connect
+        bool IsClosed = false;
+
+        bool state1 = false;
+        bool state2 = false;
+
+        bool lastBtn1 = false;
+        bool lastBtn2 = false;
+        bool resetBtn = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //configuration of arduino, enter correct serial parameter
+            Port = new System.IO.Ports.SerialPort();
+            Port.PortName = "COM3";
+            Port.BaudRate = 9600;
+            Port.ReadTimeout = 500;
+
+            try
+            {
+                Port.Open();
+            }
+            catch { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //A Thread to listen forever the serial port
+            Thread Hilo = new Thread(ListenSerial);
+            Hilo.Start();
+
+            textBox5.Invoke(new MethodInvoker(
+                delegate
+                {
+                    textBox5.BackColor = Color.Red;
+                    textBox5.Text = "OFF";
+                }
+                ));
+            textBox6.Invoke(new MethodInvoker(
+                delegate
+                {
+                    textBox6.BackColor = Color.Red;
+                    textBox6.Text = "OFF";
+                }
+                ));
+        }
+
+
+        private void ListenSerial()
+        {
+            while (!IsClosed)
+            {
+                try
+                {
+                    //read to data from arduino
+                    string AString = Port.ReadLine();
+                    AString = AString.TrimEnd('\r');
+
+                    if (AString == "BTN1")
+                    {
+                        string temp = Port.ReadLine();
+                        temp = temp.TrimEnd('\r');
+
+                        if (temp != lastBtn1.ToString() && temp == "True")
+                        {
+                            state1 = !state1;
+                            //write the data in something textbox
+                            textBox5.Invoke(new MethodInvoker(
+                                delegate
+                                {
+                                    if (state1)
+                                    {
+                                        textBox5.BackColor = Color.Green;
+                                        textBox5.Text = "ON";
+                                    }
+                                    else
+                                    {
+                                        textBox5.BackColor = Color.Red;
+                                        textBox5.Text = "OFF";
+                                    }
+                                }
+                                ));
+                        }
+                        if (temp == "True")
+                        {
+                            lastBtn1 = true;
+                        }
+                        else
+                        {
+                            lastBtn1 = false;
+                        }
+                    }
+                    else if (AString == "BTN2")
+                    {
+                        string temp = Port.ReadLine();
+                        temp = temp.TrimEnd('\r');
+
+                        if (temp != lastBtn2.ToString() && temp == "True")
+                        {
+                            state2 = !state2;
+                            //write the data in something textbox
+                            textBox6.Invoke(new MethodInvoker(
+                                delegate
+                                {
+                                    if (state2)
+                                    {
+                                        textBox6.BackColor = Color.Green;
+                                        textBox6.Text = "ON";
+                                    }
+                                    else
+                                    {
+                                        textBox6.BackColor = Color.Red;
+                                        textBox6.Text = "OFF";
+                                    }
+                                }
+                                ));
+                        }
+                        if (temp == "True")
+                        {
+                            lastBtn2 = true;
+                        }
+                        else
+                        {
+                            lastBtn2 = false;
+                        }
+                    }
+
+                    else if (AString == "BTN3")
+                    {
+                        string temp = Port.ReadLine();
+                        temp = temp.TrimEnd('\r');
+
+                        if (temp != resetBtn.ToString() && temp == "True")
+                        {
+                            state1 = false;
+                            state2 = false;
+                            textBox5.Invoke(new MethodInvoker(
+                                delegate
+                                {
+                                    if (state1)
+                                    {
+                                        textBox5.BackColor = Color.Green;
+                                        textBox5.Text = "ON";
+                                    }
+                                    else
+                                    {
+                                        textBox5.BackColor = Color.Red;
+                                        textBox5.Text = "OFF";
+                                    }
+                                }
+                                ));
+                            textBox6.Invoke(new MethodInvoker(
+                                delegate
+                                {
+                                    if (state2)
+                                    {
+                                        textBox6.BackColor = Color.Green;
+                                        textBox6.Text = "ON";
+                                    }
+                                    else
+                                    {
+                                        textBox6.BackColor = Color.Red;
+                                        textBox6.Text = "OFF";
+                                    }
+                                }
+                                ));
+                        }
+                        if (temp == "True")
+                        {
+                            resetBtn = true;
+                        }
+                        else
+                        {
+                            resetBtn = false;
+                        }
+                    }
+                    
+                    else
+                    {
+                        lastBtn1 = false;
+                        lastBtn2 = false;
+                        resetBtn = false;
+                    }
+
+                }
+                catch { }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //when the form will be closed this line close the serial port
+            IsClosed = true;
+            if (Port.IsOpen)
+                Port.Close();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
+
